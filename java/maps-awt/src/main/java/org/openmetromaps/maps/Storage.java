f47cba074359bfc41088d6969f8ce42075aeb671
Sebastian Kürten <sebastian@topobyte.de>
2017-10-02 10:38:57 +0200

    Extract save() method from SaveAction

---
+++ java/maps-awt/src/main/java/org/openmetromaps/maps/Storage.java
@@ -0,0 +1,49 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of OpenMetroMaps.
+//
+// OpenMetroMaps is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenMetroMaps is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenMetroMaps. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.maps;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.openmetromaps.maps.xml.XmlModelWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Storage
+{
+
+	final static Logger logger = LoggerFactory.getLogger(Storage.class);
+
+	public static void save(File file, MapViewer mapViewer)
+	{
+		try {
+			FileOutputStream os = new FileOutputStream(file);
+			new XmlModelWriter().write(os, mapViewer.getModel());
+			os.close();
+		} catch (ParserConfigurationException | TransformerException
+				| IOException e) {
+			logger.error("Error while saving file", e);
+			// TODO: display an error dialog
+		}
+	}
+
+}
--- java/maps-awt/src/main/java/org/openmetromaps/maps/actions/SaveAction.java
+++ java/maps-awt/src/main/java/org/openmetromaps/maps/actions/SaveAction.java
@@ -20,15 +20,11 @@ package org.openmetromaps.maps.actions;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import javax.swing.JFileChooser;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 
 import org.openmetromaps.maps.MapViewer;
-import org.openmetromaps.maps.xml.XmlModelWriter;
+import org.openmetromaps.maps.Storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -60,16 +56,7 @@ public class SaveAction extends SimpleAction
 		if (value == JFileChooser.APPROVE_OPTION) {
 			File file = chooser.getSelectedFile();
 			logger.debug("attempting to save document to file: " + file);
-
-			try {
-				FileOutputStream os = new FileOutputStream(file);
-				new XmlModelWriter().write(os, mapViewer.getModel());
-				os.close();
-			} catch (ParserConfigurationException | TransformerException
-					| IOException e) {
-				logger.error("Error while saving file", e);
-				// TODO: display an error dialog
-			}
+			Storage.save(file, mapViewer);
 		}
 	}
 

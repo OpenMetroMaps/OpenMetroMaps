8f01ecece271bb72c0257f36473ea085e92b33ae
Sebastian Kürten <sebastian@topobyte.de>
2017-10-11 12:48:29 +0200

    Implement a test that projects a number of coordinates

--- java/maps-core/src/test/java/org/openmetromap/maps/xml/TestConvertCoordinates.java
+++ java/maps-core/src/test/java/org/openmetromap/maps/xml/TestConvertCoordinates.java
@@ -32,6 +32,7 @@ import org.xml.sax.SAXException;
 import de.topobyte.adt.geo.BBox;
 import de.topobyte.adt.geo.BBoxHelper;
 import de.topobyte.adt.geo.Coordinate;
+import de.topobyte.geomath.WGS84;
 
 public class TestConvertCoordinates
 {
@@ -49,13 +50,53 @@ public class TestConvertCoordinates
 			coordinates.add(location);
 		}
 
-		convert(coordinates);
+		convert(coordinates, 1000);
 	}
 
-	private static void convert(List<Coordinate> coordinates)
+	private static void convert(List<Coordinate> coordinates, double size)
 	{
 		BBox bbox = BBoxHelper.minimumBoundingBox(coordinates);
-		System.out.println(bbox);
+		System.out.println("bbox: " + bbox);
+
+		double worldsize = 1;
+
+		double x1 = WGS84.lon2merc(bbox.getLon1(), worldsize);
+		double x2 = WGS84.lon2merc(bbox.getLon2(), worldsize);
+
+		double y1 = WGS84.lat2merc(bbox.getLat1(), worldsize);
+		double y2 = WGS84.lat2merc(bbox.getLat2(), worldsize);
+
+		double spanX = Math.abs(x1 - x2);
+		double spanY = Math.abs(y1 - y2);
+
+		double biggerSpan = Math.max(spanX, spanY);
+		double factor = size / biggerSpan;
+
+		double minX = Math.min(x1, x2);
+		double minY = Math.min(y1, y2);
+
+		System.out.println(
+				String.format("coordinates: %f,%f:%f,%f", x1, x2, y1, y2));
+		System.out.println(String.format("spanX: %f, spanY: %f", spanX, spanY));
+
+		List<Coordinate> newCoordinates = new ArrayList<>();
+
+		for (Coordinate coordinate : coordinates) {
+			double x = WGS84.lon2merc(coordinate.getLongitude(), worldsize);
+			double y = WGS84.lat2merc(coordinate.getLatitude(), worldsize);
+			double dx = x - minX;
+			double dy = y - minY;
+			double sx = dx * factor;
+			double sy = dy * factor;
+			newCoordinates.add(new Coordinate(sx, sy));
+			System.out.println(String.format("%f, %f", sx, sy));
+		}
+
+		Coordinate minimum = Coordinate.minimum(newCoordinates);
+		Coordinate maximum = Coordinate.maximum(newCoordinates);
+		System.out.println(String.format("%f:%f,%f:%f", minimum.getLongitude(),
+				maximum.getLongitude(), minimum.getLatitude(),
+				maximum.getLatitude()));
 	}
 
 }

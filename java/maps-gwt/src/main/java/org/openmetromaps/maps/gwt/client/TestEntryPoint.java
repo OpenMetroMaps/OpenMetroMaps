264fb1b19f6f7b7a86fa41631db67e7adbee2c48
Sebastian Kürten <sebastian@topobyte.de>
2017-10-20 18:41:41 +0200

    GWT: add a link to the OMM website

--- java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/TestEntryPoint.java
+++ java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/TestEntryPoint.java
@@ -18,8 +18,11 @@
 package org.openmetromaps.maps.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Float;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 
@@ -31,8 +34,22 @@ public class TestEntryPoint implements EntryPoint {
 		RootLayoutPanel.get().add(dock);
 
 		Label title = new Label("Below is a simple panel");
-		title.getElement().getStyle().setMargin(1, Unit.EM);
-		dock.addNorth(title, 2);
+		Anchor linkOMM = new Anchor("OpenMetroMaps", false, "http://www.openmetromaps.org");
+
+		FlowPanel headline = new FlowPanel();
+		headline.getElement().getStyle().setMarginTop(1, Unit.EM);
+		headline.getElement().getStyle().setMarginBottom(1, Unit.EM);
+		headline.getElement().getStyle().setMarginLeft(10, Unit.PX);
+		headline.getElement().getStyle().setMarginRight(10, Unit.PX);
+
+		headline.add(linkOMM);
+		headline.add(title);
+
+		linkOMM.getElement().getStyle().setMarginLeft(10, Unit.PX);
+
+		title.getElement().getStyle().setFloat(Float.LEFT);
+
+		dock.addNorth(headline, 2);
 
 		TestPanel test = new TestPanel();
 		dock.add(test);

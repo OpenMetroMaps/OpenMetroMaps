18812a17d4aa709820992f7158d23500d860e4ee
Sebastian Kürten <sebastian@topobyte.de>
2017-10-26 12:51:48 +0200

    JS: draft more parsing

--- javascript/parser.js
+++ javascript/parser.js
@@ -3,12 +3,25 @@ function Parser() {
     var self = this;
 
     this.stations = [];
+    this.lines = [];
+    this.viewstations = [];
 
     this.parse = function(data) {
         var $xml = $(data)
-        this.station = $xml.find('station');
-        $xml.find('station').each(function(){
+
+        var stations = $xml.find('stations');
+        stations.find('station').each(function(){
             self.stations.push($(this).attr('name'));
+        });
+
+        var lines = $xml.find('lines');
+        lines.find('line').each(function(){
+            self.lines.push($(this).attr('name'));
+        });
+
+        var view = $xml.find('view');
+        view.find('station').each(function(){
+            self.viewstations.push($(this).attr('name'));
         });
     }
 

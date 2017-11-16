c539cf9e3404243134346bca30a7afb53500c16c
Sebastian Kürten <sebastian@topobyte.de>
2017-11-16 19:09:51 +0100

    Start ScrollableAdvancedPlanPanel implementation

---
+++ java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/ScrollableAdvancedEntryPoint.java
@@ -0,0 +1,82 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of OpenMetroMaps.
+//
+// OpenMetroMaps is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenMetroMaps is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenMetroMaps. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.maps.gwt.client;
+
+import org.openmetromaps.maps.MapModel;
+import org.openmetromaps.maps.MapView;
+import org.openmetromaps.maps.xml.XmlModel;
+import org.openmetromaps.maps.xml.XmlModelConverter;
+import org.openmetromaps.maps.xml.XmlModelReader;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.XMLParser;
+
+import de.topobyte.xml.domabstraction.gwtimpl.GwtDocument;
+import de.topobyte.xml.domabstraction.iface.ParsingException;
+
+public class ScrollableAdvancedEntryPoint implements EntryPoint
+{
+
+	private ScrollableAdvancedPlanPanel panel;
+	private Label status;
+
+	@Override
+	public void onModuleLoad()
+	{
+		DockLayoutPanel dock = new DockLayoutPanel(Unit.EM);
+		RootLayoutPanel.get().add(dock);
+
+		status = new Label("Initializing...");
+		FlowPanel headline = Util
+				.headline("Below is a scrollable advanced panel", status);
+
+		dock.addNorth(headline, 2);
+
+		panel = new ScrollableAdvancedPlanPanel();
+		dock.add(panel);
+
+		String filename = "berlin.xml";
+		Util.load(filename, xml -> parseXml(xml));
+	}
+
+	protected void parseXml(String xml)
+	{
+		Document doc = XMLParser.parse(xml);
+		GwtDocument gwtDoc = new GwtDocument(doc);
+		try {
+			XmlModel xmlModel = XmlModelReader.read(gwtDoc);
+			status.setText("stations: " + xmlModel.getStations().size());
+			MapModel mapModel = new XmlModelConverter().convert(xmlModel);
+			MapView view = mapModel.getViews().get(0);
+			String name = view.getName();
+			status.setText("view: " + name);
+			panel.setModel(mapModel);
+			panel.render();
+		} catch (ParsingException e) {
+			Window.alert("error while parsing document");
+		}
+	}
+
+}
---
+++ java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/ScrollableAdvancedPlanPanel.java
@@ -0,0 +1,104 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of OpenMetroMaps.
+//
+// OpenMetroMaps is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenMetroMaps is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenMetroMaps. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.maps.gwt.client;
+
+import org.openmetromaps.maps.MapModel;
+import org.openmetromaps.maps.MapView;
+import org.openmetromaps.maps.MapViewStatus;
+import org.openmetromaps.maps.PlanRenderer;
+import org.openmetromaps.maps.PlanRenderer.SegmentMode;
+import org.openmetromaps.maps.PlanRenderer.StationMode;
+import org.openmetromaps.maps.graph.LineNetwork;
+import org.openmetromaps.maps.painting.core.GenericPaintFactory;
+import org.openmetromaps.maps.painting.core.Painter;
+import org.openmetromaps.maps.painting.gwt.GwtPainter;
+
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.canvas.dom.client.CssColor;
+
+public class ScrollableAdvancedPlanPanel extends BaseMapWindowPanel
+{
+
+	private MapModel mapModel;
+	private MapView mapView;
+	private LineNetwork lineNetwork;
+
+	private MapViewStatus mapViewStatus;
+	private StationMode stationMode = StationMode.SIMPLE;
+	private SegmentMode segmentMode = SegmentMode.STRAIGHT;
+
+	private PlanRenderer renderer;
+
+	public ScrollableAdvancedPlanPanel()
+	{
+		// TODO: use start position
+
+		// ViewActions.setupMovementActions(getInputMap(), getActionMap(),
+		// this);
+
+		MouseProcessor panMouseHandler = new PanMouseProcessor<>(this);
+		MouseProcessor zoomMouseHandler = new ZoomMouseProcessor<>(this);
+		Util.addHandler(canvas, panMouseHandler);
+		Util.addHandler(canvas, zoomMouseHandler);
+	}
+
+	public void setModel(MapModel mapModel)
+	{
+		this.mapModel = mapModel;
+		mapView = mapModel.getViews().get(0);
+		lineNetwork = mapView.getLineNetwork();
+
+		setScene(mapView.getConfig().getScene());
+
+		initRenderer();
+	}
+
+	private void initRenderer()
+	{
+		renderer = new PlanRenderer(lineNetwork, mapViewStatus, stationMode,
+				segmentMode, this, this, 1, new GenericPaintFactory());
+	}
+
+	@Override
+	public void render()
+	{
+		// g.setColor(Color.WHITE);
+		// fillRect(g, scene.getX1(), scene.getY1(), scene.getX2(),
+		// scene.getY2());
+
+		Context2d c = canvas.getContext2d();
+		fillBackground(c);
+		renderContent(c);
+	}
+
+	private void fillBackground(Context2d c)
+	{
+		c.setFillStyle(CssColor.make("#eeeeee"));
+		c.fillRect(0, 0, width, height);
+	}
+
+	private void renderContent(Context2d c)
+	{
+		Painter painter = new GwtPainter(c);
+
+		fillBackground(c);
+
+		renderer.paint(painter);
+	}
+
+}

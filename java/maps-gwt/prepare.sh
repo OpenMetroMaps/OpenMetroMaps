2d31c0aabe2d00312614719b36ac05d12df4b829
Sebastian Kürten <sebastian@topobyte.de>
2017-11-20 16:23:25 +0100

    GWT: fix path to script in prepare.sh

--- java/maps-gwt/prepare.sh
+++ java/maps-gwt/prepare.sh
@@ -5,9 +5,9 @@ gradle depunpack
 DIR="build/unpackedJars"
 
 cp UnpackedJars.gwt.xml "$DIR"
-~/github/sebkur/javaparser-transform-tests/scripts/remove-externalizable.sh "$DIR"
-~/github/sebkur/javaparser-transform-tests/scripts/replace-string-format.sh "$DIR"
-~/github/sebkur/javaparser-transform-tests/scripts/replace-method.sh clone \
+~/github/sebkur/javaparser-transforms/scripts/remove-externalizable.sh "$DIR"
+~/github/sebkur/javaparser-transforms/scripts/replace-string-format.sh "$DIR"
+~/github/sebkur/javaparser-transforms/scripts/replace-method.sh clone \
     "public Object clone() { return new Coordinate(this); }" \
     "$DIR/com/vividsolutions/jts/geom/Coordinate.java"
 rm "$DIR/jama/MatrixIO.java"

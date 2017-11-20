b121971f09e5aecac0a02217f9097291e605f15f
Sebastian Kürten <sebastian@topobyte.de>
2017-11-20 19:17:50 +0100

    GWT: make prepare.sh easier configurable

--- java/maps-gwt/prepare.sh
+++ java/maps-gwt/prepare.sh
@@ -2,43 +2,44 @@
 
 gradle depunpack
 
+JT="$HOME/github/sebkur/javaparser-transforms/scripts"
 DIR="build/unpackedJars"
 
 cp UnpackedJars.gwt.xml "$DIR"
 
 rm "$DIR/jama/MatrixIO.java"
 
-~/github/sebkur/javaparser-transforms/scripts/remove-externalizable.sh "$DIR"
-~/github/sebkur/javaparser-transforms/scripts/replace-string-format.sh "$DIR"
+"$JT/remove-externalizable.sh" "$DIR"
+"$JT/replace-string-format.sh" "$DIR"
 
 COORDINATE="$DIR/com/vividsolutions/jts/geom/Coordinate.java"
-~/github/sebkur/javaparser-transforms/scripts/replace-method.sh clone \
+"$JT/replace-method.sh" clone \
     "public Object clone() { return new Coordinate(this); }" \
     "$COORDINATE"
 
 VECTOR="$DIR/de/topobyte/lightgeom/lina/Vector2.java"
-~/github/sebkur/javaparser-transforms/scripts/remove-method.sh fastInverseNorm "$VECTOR"
-~/github/sebkur/javaparser-transforms/scripts/remove-method.sh fastInverseSquareRoot "$VECTOR"
-~/github/sebkur/javaparser-transforms/scripts/remove-method.sh normalizeFast "$VECTOR"
+"$JT/remove-method.sh" fastInverseNorm "$VECTOR"
+"$JT/remove-method.sh" fastInverseSquareRoot "$VECTOR"
+"$JT/remove-method.sh" normalizeFast "$VECTOR"
 
 BBOX="$DIR/de/topobyte/adt/geo/BBox.java"
-~/github/sebkur/javaparser-transforms/scripts/remove-method.sh toEnvelope "$BBOX"
-~/github/sebkur/javaparser-transforms/scripts/remove-constructor.sh Envelope "$BBOX"
-~/github/sebkur/javaparser-transforms/scripts/remove-import.sh Envelope "$BBOX"
+"$JT/remove-method.sh" toEnvelope "$BBOX"
+"$JT/remove-constructor.sh" Envelope "$BBOX"
+"$JT/remove-import.sh" Envelope "$BBOX"
 
 JAMA_MATRIX="$DIR/jama/Matrix.java"
-~/github/sebkur/javaparser-transforms/scripts/remove-method-annotation.sh clone Override "$JAMA_MATRIX"
+"$JT/remove-method-annotation.sh" clone Override "$JAMA_MATRIX"
 
 MATRIX="$DIR/de/topobyte/lina/Matrix.java"
-~/github/sebkur/javaparser-transforms/scripts/substitute.sh \
+"$JT/substitute.sh" \
     "System.getProperty(\"line.separator\")" "\"\n\"" "$MATRIX"
 
 TINTOBJECTHASHMAP="$DIR/com/slimjars/dist/gnu/trove/map/hash/TIntObjectHashMap.java"
-~/github/sebkur/javaparser-transforms/scripts/substitute.sh \
+"$JT/substitute.sh" \
     "dest = (V[]) java.lang.reflect.Array.newInstance(dest.getClass().getComponentType(), _size);" \
     "dest = (V[]) new Object[_size];" \
     "$TINTOBJECTHASHMAP"
-~/github/sebkur/javaparser-transforms/scripts/substitute.sh \
+"$JT/substitute.sh" \
     "a = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);" \
     "a = (T[]) new Object[size];" \
     "$TINTOBJECTHASHMAP"

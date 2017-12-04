fc5c69a91067da86939762b109cec5d76605fd25
Sebastian Kürten <sebastian@topobyte.de>
2017-12-04 15:09:28 +0100

    GWT: improve DemoEntryPoint
    Dont let DemoEntryPoint extend ScrollableEntryPoint. That class is
    tailored for use by the testing entry points and not a good base for
    this more flexible component.

--- java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/DemoEntryPoint.java
+++ java/maps-gwt/src/main/java/org/openmetromaps/maps/gwt/client/DemoEntryPoint.java
@@ -20,29 +20,125 @@ package org.openmetromaps.maps.gwt.client;
 import java.util.Map;
 
 import org.openmetromaps.maps.MapModel;
+import org.openmetromaps.maps.MapView;
+import org.openmetromaps.maps.xml.XmlModel;
+import org.openmetromaps.maps.xml.XmlModelConverter;
+import org.openmetromaps.maps.xml.XmlModelReader;
 
-public class DemoEntryPoint extends ScrollableEntryPoint
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.XMLParser;
+
+import de.topobyte.xml.domabstraction.gwtimpl.GwtDocument;
+import de.topobyte.xml.domabstraction.iface.ParsingException;
+
+public class DemoEntryPoint implements EntryPoint
 {
 
 	private ScrollableAdvancedPlanPanel panel;
+	private Label status;
 
-	@Override
-	protected BaseMapWindowPanel createPanel()
-	{
-		panel = new ScrollableAdvancedPlanPanel();
-		return panel;
-	}
-
-	@Override
 	protected void setParameters(Map<String, String> params)
 	{
 		panel.setDebugSize(Util.getBoolean(params, "debug-size", false));
 	}
 
-	@Override
 	protected void setModel(MapModel mapModel)
 	{
 		panel.setModel(mapModel);
+	}
+
+	@Override
+	public void onModuleLoad()
+	{
+		RootPanel root = RootPanel.get("gwt");
+
+		ResizingAbsolutePanel main = new ResizingAbsolutePanel();
+		root.add(main);
+		StyleUtil.absolute(main, 0, 0, 0, 0, Unit.PX);
+
+		DockLayoutPanel dock = new DockLayoutPanel(Unit.EM);
+		StyleUtil.absolute(dock, 0, 0, 0, 0, Unit.PX);
+		main.add(dock);
+
+		status = new Label("Initializing...");
+
+		panel = new ScrollableAdvancedPlanPanel();
+		dock.add(panel);
+
+		addButtons(main);
+
+		Map<String, String> params = Util.loadParameters("parameters");
+
+		setParameters(params);
+
+		String filename = params.get("file");
+		Util.load(filename, xml -> parseXml(xml));
+	}
+
+	private void addButtons(AbsolutePanel main)
+	{
+		VerticalPanel buttons = new VerticalPanel();
+		StyleUtil.absoluteTopRight(buttons, 1, 1, Unit.EM);
+		main.add(buttons);
+
+		Button buttonIn = new Button("+");
+		StyleUtil.setHeight(buttonIn, 3, Unit.EM);
+		StyleUtil.setWidth(buttonIn, 100, Unit.PCT);
+		StyleUtil.setProperty(buttonIn, "minWidth", 3, Unit.EM);
+		buttons.add(buttonIn);
+
+		Button buttonOut = new Button("-");
+		StyleUtil.setHeight(buttonOut, 3, Unit.EM);
+		StyleUtil.setWidth(buttonOut, 100, Unit.PCT);
+		StyleUtil.setProperty(buttonOut, "minWidth", 3, Unit.EM);
+		buttons.add(buttonOut);
+
+		StyleUtil.marginTop(buttonOut, 0.5, Unit.EM);
+
+		buttonIn.addClickHandler(e -> {
+			zoomIn();
+		});
+
+		buttonOut.addClickHandler(e -> {
+			zoomOut();
+		});
+	}
+
+	protected void zoomIn()
+	{
+		panel.setZoom(panel.getZoom() * 1.1);
+	}
+
+	protected void zoomOut()
+	{
+		panel.setZoom(panel.getZoom() / 1.1);
+	}
+
+	protected void parseXml(String xml)
+	{
+		Document doc = XMLParser.parse(xml);
+		GwtDocument gwtDoc = new GwtDocument(doc);
+		try {
+			XmlModel xmlModel = XmlModelReader.read(gwtDoc);
+			status.setText("stations: " + xmlModel.getStations().size());
+			MapModel mapModel = new XmlModelConverter().convert(xmlModel);
+			MapView view = mapModel.getViews().get(0);
+			String name = view.getName();
+			status.setText("view: " + name);
+			setModel(mapModel);
+			panel.render();
+		} catch (ParsingException e) {
+			Window.alert("error while parsing document");
+		}
 	}
 
 }

394ac051661940d0cdcd491f47b83e5704234391
Sebastian Kürten <sebastian@topobyte.de>
2018-01-08 11:06:22 +0100

    ChangeModel CSV export: build map of station names/ids

--- java/change-model-csv-export/src/test/java/org/openmetromaps/change/TestConvertChangeModelBerlin.java
+++ java/change-model-csv-export/src/test/java/org/openmetromaps/change/TestConvertChangeModelBerlin.java
@@ -21,7 +21,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -37,11 +39,16 @@ import org.openmetromaps.maps.xml.XmlModelConverter;
 import org.openmetromaps.misc.NameChanger;
 import org.openmetromaps.rawchange.RawChangeModel;
 import org.openmetromaps.rawchange.xml.DesktopXmlChangeReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.topobyte.xml.domabstraction.iface.ParsingException;
 
 public class TestConvertChangeModelBerlin
 {
+
+	final static Logger logger = LoggerFactory
+			.getLogger(TestConvertChangeModelBerlin.class);
 
 	public static void main(String[] args)
 			throws ParserConfigurationException, IOException, ParsingException
@@ -63,11 +70,22 @@ public class TestConvertChangeModelBerlin
 
 		NameChanger nameChanger = TestData.berlinGtfsNameChanger();
 
+		Map<String, String> nameToId = new HashMap<>();
+
 		GtfsZip gtfs = new GtfsZip(pathGtfs);
 		List<Stop> stops = gtfs.readStops();
 		for (Stop stop : stops) {
 			String name = nameChanger.applyNameFixes(stop.getName());
-			System.out.println(String.format("%s: %s", stop.getId(), name));
+			if (stop.getLocationType().equals("1")) {
+				if (nameToId.containsKey(name)) {
+					String dup = nameToId.get(name);
+					logger.warn(
+							String.format("duplicate GTFS station %s → %s: %s",
+									stop.getId(), dup, name));
+				} else {
+					nameToId.put(name, stop.getId());
+				}
+			}
 		}
 		gtfs.close();
 

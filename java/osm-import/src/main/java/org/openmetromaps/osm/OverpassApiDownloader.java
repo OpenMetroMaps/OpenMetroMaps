fe4b2fa81cf5ca452a0371e41662127f961d2fef
Sebastian Kürten <sebastian@topobyte.de>
2018-02-24 13:00:33 +0100

    Add class for downloading data from Overpass API

---
+++ java/osm-import/src/main/java/org/openmetromaps/osm/OverpassApiDownloader.java
@@ -0,0 +1,83 @@
+// Copyright 2018 Sebastian Kuerten
+//
+// This file is part of OpenMetroMaps.
+//
+// OpenMetroMaps is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenMetroMaps is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenMetroMaps. If not, see <http://www.gnu.org/licenses/>.
+
+package org.openmetromaps.osm;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class OverpassApiDownloader
+{
+
+	public void execute(String q, Path path)
+			throws MalformedURLException, IOException
+	{
+		OutputStream output = Files.newOutputStream(path);
+		execute(q, output);
+		output.close();
+	}
+
+	public void execute(String q, OutputStream output)
+			throws MalformedURLException, IOException
+	{
+		HttpPost post = new HttpPost(
+				"http://www.overpass-api.de/api/interpreter");
+
+		post.setEntity(new StringEntity(q));
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		ResponseHandler<Void> handler = new ResponseHandler<Void>() {
+
+			@Override
+			public Void handleResponse(HttpResponse response)
+					throws ClientProtocolException, IOException
+			{
+
+				int status = response.getStatusLine().getStatusCode();
+				if (status == 200) {
+					HttpEntity entity = response.getEntity();
+					InputStream input = entity.getContent();
+
+					IOUtils.copy(input, output);
+					return null;
+				} else {
+					throw new ClientProtocolException(
+							"Unexpected response status: " + status);
+				}
+			}
+
+		};
+
+		httpclient.execute(post, handler);
+	}
+
+}

35ccd2d6449db645513dcca5371a219b5d851700
Sebastian Kürten <sebastian@topobyte.de>
2025-08-02 10:49:48 +0200

    GWT: make sure to rebuild fully with update.sh
    When not deleting the unit cache and other intermediate build files, it
    seems some changes like to the *.gwt.xml file do not take effect.

--- gwt/update.sh
+++ gwt/update.sh
@@ -3,7 +3,8 @@
 set -e
 
 DIR=$(dirname $0)
-OUT="build/gwt/war"
+OUT="build/gwt"
+WAR="$OUT/war"
 
 rm -rf "$OUT"
 rm -rf test
@@ -12,7 +13,7 @@ mkdir test
 
 OUTPUT="demo simple scrollable_simple scrollable_advanced"
 for DIR in $OUTPUT; do
-	cp -a "$OUT/$DIR" test
+	cp -a "$WAR/$DIR" test
 done
 
 cp src/main/webapp/berlin.xml src/main/webapp/*.html test
